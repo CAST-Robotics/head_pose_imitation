67f029ebeeaf519ff6850180b2e2aee645818bc0
amirhosein-vedadi <amirhsein.vedadi@gmail.com>
2023-08-10 17:18:07 +0330

    fix: multi detection error fixed

--- scripts/imitation.py
+++ scripts/imitation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import argparse
 import service
 import cv2
 import roslib
@@ -13,14 +12,20 @@ import numpy as np
 def main(color=(224, 255, 255)):
 
     config_path = roslib.packages.get_pkg_dir('head_pose_imitation') + "/config"
-    print(config_path)
 
     fd = service.UltraLightFaceDetecion(config_path + "/weights/RFB-320.tflite",
                                        conf_threshold=0.95)
 
     fa = service.DepthFacialLandmarks(config_path + "/weights/sparse_face.tflite")
 
-    handler = getattr(service, "pose")
+    # Constants for mapping detected angles space to motor commands space
+    pitch_range = [-30, 30]
+    roll_range = [-50, 50]
+    yaw_range = [-90, 90]
+    pitch_command_range = [180, 110]
+    roll_command_range = [100, 190]
+    yaw_command_range = [90, 210]
+
     cap = cv2.VideoCapture(0)
     command_array = Int32MultiArray()
     command_array.data = [0] * 23
@@ -42,21 +47,22 @@ def main(color=(224, 255, 255)):
 
         # raw copy for reconstruction
         feed = frame.copy()
+        if len(boxes) > 0:
+            bbox = [boxes[0]]
+            for results in fa.get_landmarks(feed, bbox):
+                euler = service.pose(frame, results, color)
+                euler_queue.append(euler)  # Add euler angles to the moving average queue
 
-        for results in fa.get_landmarks(feed, boxes):
-            euler = handler(frame, results, color)
-            euler_queue.append(euler)  # Add euler angles to the moving average queue
-
-        if len(euler_queue) > 0:
-            averaged_euler = np.mean(euler_queue, axis=0)  # Calculate the average of euler angles
-            print(f"Pitch: {averaged_euler[0]}; Yaw: {averaged_euler[1]}; Roll: {averaged_euler[2]};")
-            pitch = 110 + (180 - 110) * ((averaged_euler[0] - (-30)) / (60 - (-30)))
-            roll = 100 + (190 - 100) * ((-averaged_euler[2] - (-50)) / (50 - (-50)))
-            yaw = 90 + (210 - 90) * ((-averaged_euler[1] - (-90)) / (90 - (-90)))
-            command_array.data[20] = int(pitch)
-            command_array.data[21] = int(roll)
-            command_array.data[22] = int(yaw)
-            publisher.publish(command_array)
+            if len(euler_queue) > 0:
+                averaged_euler = np.mean(euler_queue, axis=0)  # Calculate the average of euler angles
+                print(f"Pitch: {averaged_euler[0]}; Yaw: {averaged_euler[1]}; Roll: {averaged_euler[2]};")
+                pitch = pitch_command_range[0] + (pitch_command_range[1] - pitch_command_range[0]) * ((-averaged_euler[0] - pitch_range[0]) / (pitch_range[1] - pitch_range[0]))
+                roll = roll_command_range[0] + (roll_command_range[1] - roll_command_range[0]) * ((-averaged_euler[2] - (roll_range[0])) / (roll_range[1] - (roll_range[0])))
+                yaw = yaw_command_range[0] + (yaw_command_range[1] - yaw_command_range[0]) * ((-averaged_euler[1] - yaw_range[0]) / (yaw_range[1] - yaw_range[0]))
+                command_array.data[20] = int(pitch)
+                command_array.data[21] = int(roll)
+                command_array.data[22] = int(yaw)
+                publisher.publish(command_array)
 
         cv2.imshow("demo", frame)
         if cv2.waitKey(1) == ord("q"):
